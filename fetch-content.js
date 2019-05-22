8b9702317308836b710e78966d78f43f43c20fc7
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-05-22 20:54:33 +0200

    wip

--- fetch-content.js
+++ fetch-content.js
@@ -1,43 +1,25 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
-const packages = [
-  {
-    name: "enum",
-    repository: "sebastiandedeyne/docs-dummy-repo-1",
-    branches: {
-      master: "v2",
-      v1: "v1"
-    }
-  },
-  {
-    name: "another-package",
-    repository: "sebastiandedeyne/docs-dummy-repo-1",
-    branches: {
-      master: "v2",
-      v1: "v1"
-    }
-  }
-];
+const repositories = require("./repositories.json");
 
-(async function() {
-  await exec("rm -rf content");
-  await exec("mkdir temp");
+(async function () {
+    await exec("rm -rf content");
+    await exec("mkdir temp");
 
-  try {
-    for (const pkg of packages) {
-      await exec(`mkdir -p content/${pkg.name}`);
-      await exec(
-        `git clone https://github.com/${pkg.repository}.git temp/${pkg.name}`
-      );
+    try {
+        for (const repository of repositories) {
+            await exec(`mkdir -p content/${repository.name}`);
+            await exec(`git clone https://github.com/${repository.repository}.git temp/${repository.name}`);
 
-      for (const [branch, alias] of Object.entries(pkg.branches)) {
-        await exec(`cd temp/${pkg.name} && git checkout ${branch}`);
-        await exec(`cp -r temp/${pkg.name}/docs content/${pkg.name}/${alias}`);
-      }
+            for (const [branch, alias] of Object.entries(repository.branches)) {
+                await exec(`cd temp/${repository.name} && git checkout ${branch}`);
+                await exec(`cp -r temp/${repository.name}/docs content/${repository.name}/${alias}`);
+            }
+        }
+    } catch (error) {
+        throw error;
+    } finally {
+        await exec("rm -rf temp");
     }
-  } catch (error) {
-  } finally {
-    await exec("rm -rf temp");
-  }
 })();
