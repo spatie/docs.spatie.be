c78df6995946bfed8c404d84c0be6c3ed911fea6
Rias <hello@rias.be>
2019-05-23 12:14:48 +0200

    Time fetching repos

--- fetch-content.js
+++ fetch-content.js
@@ -1,5 +1,7 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
+console.log('Fetching repositories...');
+console.time('Fetched repositories');
 
 const repositories = require("./repositories.json");
 
@@ -8,20 +10,15 @@ function transformBranchToFolderName(branch) {
 }
 
 (async function () {
-    await exec("rm -rf content");
+    let promises = [];
 
-    try {
-        let promises = [];
-
-        for (const repository of repositories) {
-            for (const [branch, alias] of Object.entries(repository.branches)) {
-                promises.push(exec(`mkdir -p content/${repository.name}/${alias} && curl https://codeload.github.com/${repository.repository}/tar.gz/${branch} \
-                 | tar -xz -C content/${repository.name}/${alias} --strip=2 ${repository.repository.split('/')[1]}-${transformBranchToFolderName(branch)}/docs`));
-            }
+    for (const repository of repositories) {
+        for (const [branch, alias] of Object.entries(repository.branches)) {
+            promises.push(exec(`mkdir -p content/${repository.name}/${alias} && curl https://codeload.github.com/${repository.repository}/tar.gz/${branch} \
+             | tar -xz -C content/${repository.name}/${alias} --strip=2 ${repository.repository.split('/')[1]}-${transformBranchToFolderName(branch)}/docs`));
         }
-
-        await Promise.all(promises);
-    } catch (error) {
-        throw error;
     }
+
+    await Promise.all(promises);
+    console.timeEnd('Fetched repositories');
 })();
