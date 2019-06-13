f7fed7a9b6fb186c358608a221f8c76ce4d80098
Rias <hello@rias.be>
2019-06-13 13:59:12 +0200

    Add categories + anchor links

--- fetch-content.js
+++ fetch-content.js
@@ -15,7 +15,8 @@ function transformBranchToFolderName(branch) {
     for (const repository of repositories) {
         for (const [branch, alias] of Object.entries(repository.branches)) {
             promises.push(exec(`mkdir -p content/${repository.name}/${alias} && curl https://codeload.github.com/${repository.repository}/tar.gz/${branch} \
-             | tar -xz -C content/${repository.name}/${alias} --strip=2 ${repository.repository.split('/')[1]}-${transformBranchToFolderName(branch)}/docs`));
+             | tar -xz -C content/${repository.name}/${alias} --strip=2 ${repository.repository.split('/')[1]}-${transformBranchToFolderName(branch)}/docs \
+             && echo "---\ntitle: ${repository.name}\ncategory: ${repository.category}\n---" > content/${repository.name}/_index.md`));
         }
     }
 
